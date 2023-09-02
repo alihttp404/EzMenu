3d13ad4b70d03076f1bede0413d7398bf94df12f
Ali Mammadov <ali.mammadov@easlearn.com>
2023-09-02 14:23:41 +0400

    Add project files.

---
+++ EzMenu/Program.cs
@@ -0,0 +1,56 @@
+﻿using EzMenu;
+
+Menu menu = new Menu(new List<string>
+{
+    "Option 1",
+    "Option 2",
+    "Option 3",
+    "Option 4",
+    "Option 5",
+    "Exit"
+});
+
+while (true)
+{
+    int option = menu.Start();
+    switch (option)
+    {
+        case 0:
+            Console.WriteLine("Option 1 chosen!");
+            Console.ReadKey(false);
+            break;
+
+        case 1:
+            Console.WriteLine("Option 2 chosen!");
+            Console.ReadKey(false);
+            break;
+
+        case 2:
+            Console.WriteLine("Option 3 chosen!");
+            Console.ReadKey(false);
+            break;
+
+        case 3:
+            Console.WriteLine("Option 4 chosen!");
+            Console.ReadKey(false);
+            break;
+
+        case 4:
+            Console.WriteLine("Option 5 chosen!");
+            Console.ReadKey(false);
+            break;
+
+        case 5:
+            Console.WriteLine("Exiting...");
+            Thread.Sleep(2000);
+            return;
+
+        case (-1):
+            Console.WriteLine("Exiting...");
+            Thread.Sleep(2000);
+            return;
+
+        default:
+            break;
+    }
+}
